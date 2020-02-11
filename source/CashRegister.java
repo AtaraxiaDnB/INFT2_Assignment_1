930571e21bcdbf7de8c413108321e71cd39a9d0b
Aisha <aisha.king1994@gmail.com>
2020-02-11 10:31:40 +1030

    fix: Added dollar signs to print statements
    Because:
    -Print statements did not contain dollar signs
    
    Just added a
    dollar sign at the end of each message before the value is printed, that
    way it appears as if if it is a cash value.
    
    Closes No dollar signs before cash values

--- source/CashRegister.java
+++ source/CashRegister.java
@@ -9,27 +9,27 @@ public class CashRegister
 
       Scanner in = new Scanner(System.in);
 
-      System.out.print("Please enter cash register's float:");
+      System.out.print("Please enter cash register's float: $");
       s = in.nextLine();
       balance = Double.parseDouble(s);
 
       System.out.print("Please enter the item's name:");
       s = in.nextLine();
 
-      System.out.print("Please enter the item's cost:");
+      System.out.print("Please enter the item's cost: $");
       c = in.nextLine();
 
       Transaction trans = new Transaction(s, Double.parseDouble(c));
 
-      System.out.print("Please enter the cash amount tendered:");
+      System.out.print("Please enter the cash amount tendered: $");
       s = in.nextLine();
       c = Double.toString(Double.parseDouble(s) - trans.getCost());
 
-      System.out.println("Amount of change required = " + c);
+      System.out.println("Amount of change required: $" + c);
 
       c = Double.toString(balance + trans.getCost());
 
-      System.out.println("Balance of the Cash Register:" + c);
+      System.out.println("Balance of the Cash Register: $" + c);
 
 
 
